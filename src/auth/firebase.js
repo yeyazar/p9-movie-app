6474896860204d584bf880c1db62e02f1e7fd829
Yavuz <52497034+yeyazar@users.noreply.github.com>
2023-03-13 19:49:40 +0100

    firebase comments

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,6 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+// https://firebase.google.com/docs/auth/web/start
+// https://console.firebase.google.com/ => project settings
+//! get firebaseconfig settings from firebase console settings 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_apiKey,
@@ -16,4 +19,4 @@ const app = initializeApp(firebaseConfig);
 
 
 // Initialize Firebase Authentication and get a reference to the service
-const auth = getAuth(app);
+export const auth = getAuth(app);
