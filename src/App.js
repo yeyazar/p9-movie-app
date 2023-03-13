756ededa1cf159aff0708afbf0dd0e9994dfbf32
Yavuz <52497034+yeyazar@users.noreply.github.com>
2023-03-13 18:33:04 +0100

    fixednavbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 import Register from "./pages/Register";
 
 const App = () => {
   return (
     <div>
+      <Navbar/>
       <Register />
     </div>
   );
