889b1e9e95e874b0384b2ac41fad3c497023b46d
Yavuz <52497034+yeyazar@users.noreply.github.com>
2023-03-13 18:46:50 +0100

    approuter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Register from "./pages/Register";
+import AppRouter from "./router/AppRouter";
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <Register />
+      <AppRouter/>
     </div>
   );
 };
