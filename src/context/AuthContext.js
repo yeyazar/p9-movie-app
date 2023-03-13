c051ff467d91cde4f632cfe6de0df55f5fd9df9d
Yavuz <52497034+yeyazar@users.noreply.github.com>
2023-03-13 20:27:23 +0100

    v9

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,6 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { createContext } from "react";
+import { auth } from "../auth/firebase";
 
 //export const {Provider} = createContext()
 export const AuthContext = createContext();
@@ -9,6 +11,21 @@ export const AuthContext = createContext();
 
 const AuthContextProvider = ({ children }) => {
   const values = { currentUser: { displayName: "Vega Xagev" } };
+
+  const createUser = async (email, password) => {
+    try {
+      //? firebase method to create a new User
+      let userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(userCredential);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
 };
 
